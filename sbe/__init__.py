0b02b869203f3711c4b4a8182e2db4a0db0e8228
Khush Jain <khush@orbitmarkets.io>
2025-06-26 22:25:34 +0530

    Fix decoding primitive types

--- sbe/__init__.py
+++ sbe/__init__.py
@@ -1040,7 +1040,7 @@ def _walk_fields_decode(schema: Schema, rv: dict, fields: List[Union[Group, Fiel
             _walk_fields_decode_composite(schema, rv[f.name], f.type, vals, cursor)
 
         else:
-            if f.type.presence != Presence.CONSTANT:
+            if isinstance(f.type, PrimitiveType) or f.type.presence != Presence.CONSTANT:
                 _decode_value(schema, rv, f.name, f.type, vals, cursor)
 
 def _parse_schema(f: TextIO) -> Schema:
